89b578b78c7f14bd256fb11fe8ff4a4a119471d4
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-08 16:57:21 -0700

    Prepare for initial Heroku push

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 import SignupFormContainer from "./session_form/signup_form_container";
 import LoginFormContainer from "./session_form/login_form_container";
-import NavbarContainer from './navbar/navbar_container';
-
+import NavbarContainer from "./navbar/navbar_container";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 const App = () => (
   <div>
     <NavbarContainer />
-    <Route path="/signup" component={ SignupFormContainer } />
-    <Route path="/login" component={ LoginFormContainer } />
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      {/* <Route exact path="/" component={SplashContainer} /> */}
+    </Switch>
   </div>
 );
 
