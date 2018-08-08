a90fed562d7536dd3f29c848dbbc1db4e3a5f0f8
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-08 12:08:17 -0700

    Update session form with turnary

---
+++ frontend/components/session_form/signup_form_container.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { signup } from "../../actions/session_actions";
+import SessionForm from "./session_form";
+
+const mapStateToProps = state => {
+  return {
+    errors: state.errors.session,
+    formType: "signup"
+    // navLink: <Link to="/login">log in instead</Link>
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: user => dispatch(signup(user))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
