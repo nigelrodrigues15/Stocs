00130cbf140f10d7156f4adf933525a7661c5d5d
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-07 17:17:25 -0700

    create users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
+    
+    validates :username, :password_digest, :session_token, presence: true
+    validates :fname, :lname, :email, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 1, allow_nil: true}
+    
+    attr_reader :password
+    after_initialize :ensure_session_token
+  
+    def self.find_by_credentials(username, password)
+      @user = User.find_by(username: username)
+      @user.is_password?(password) ? @user : nil
+    end
+  
+    def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  
+    def is_password?(password)
+      BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+  
+    def reset_session_token
+      self.session_token = SecureRandom.urlsafe_base64
+      self.save!
+      self.session_token
+    end
+  
+    private
+  
+    def ensure_session_token
+      self.reset_session_token unless self.session_token
+    end
+
+  
 end
