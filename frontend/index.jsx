e5a26a9a28691e9eb3ac6c9b4d8cdd071062dd05
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-07 18:28:24 -0700

    working limited userAuth

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Temporary for testing
+import { login, logout, signup } from './util/session_api_util';
+//End testing
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Its working :) </h1>, root);
+
+    //Temporary for testing
+    window.login = login;
+    window.logout = logout;
+    window.signup = signup;
+    //End testing
+    
 });
